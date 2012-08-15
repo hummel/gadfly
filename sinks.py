1e05b070c8a875bc9d8da1acc811068473c8e210
Jacob Hummel <jhummel@astro.as.utexas.edu>
2012-08-15 17:33:59 -0500

    Making sinks.py into something useable -- checkpoint.

--- sinks.py
+++ sinks.py
@@ -3,32 +3,42 @@
 """
 Classes and routines for analyzing sink data output by gadget.
 """
-import os
 import numpy as np
 import asciitable
-from matplotlib import pyplot
 #===============================================================================
 
-def plot_sinkdat(path, write_dir):
-    
-    # Code unit conversions
-    unitMass_g = 1.989e43 # g
-    unitLength_cm = 3.085678e21 # cm
-    unitVelocity_cgs= 1.0e5
-    unitDensity_cgs= unitMass_g / unitLength_cm**3
-    unitTime_s= unitLength_cm / unitVelocity_cgs
-    unitDensity_cgs= unitMass_g / unitLength_cm**3
-    unitPressure_cgs= unitMass_g / unitLength_cm/ unitTime_s**2
-    unitEnergy_cgs= unitMass_g * unitLength_cm**2 / unitTime_s**2
-    # Fundamental constants
-    k_B = 1.3806e-16 # erg/K
-    m_H = 1.6726e-24 # g
-    GRAVITY = 6.6726e-8 # dyne * cm**2 / g**2
-    G = GRAVITY / unitLength_cm**3 * unitMass_g * unitTime_s**2
-    X_h = 0.76 # Hydrogen Mass Fraction
+class SinkData(object):
+    def __init__(self,filename):
+        super(SinkData,self).__init__()
+        ### Read in the data
+        try:
+            sinkdata = asciitable.read(filename)
+        except IOError:
+            print "Specified sinkdata file not found!"
 
-    ### Read in the data
-    sinkdata = asciitable.read(path)
+        self.time = sinkdata['col1']
+        self.npart_acc = sinkdata['col2']
+        self.r_sink_phys = sinkdata['col3']
+        self.part_internal_energy = sinkdata['col4']
+        self.sink_entropy = sinkdata['col5']
+        self.part_id = sinkdata['col6']
+        self.sink_id  = sinkdata['col7']
+        self.sink_pressure = sinkdata['col8']
+
+class SingleSink(SinkData):
+    def __init__(self, filename, number, sink_id=False):
+        super(SingleSink,self).__init__(filename)
+        self.unique_id, unique_index = np.unique(self.sink_id,
+                                                      return_index=True)
+        if number <= self.unique_id.size:
+            self.sink_num = number
+        else:
+            raise KeyError
+        
+
+
+        '''
+
     # Select only main sink
     sink0 = sinkdata['col7'][0]
     sinkdata = sinkdata[np.where(sinkdata['col7'] == sink0)]
@@ -41,78 +51,4 @@ def plot_sinkdat(path, write_dir):
         selection.append(times[-1])
     sinkdata = sinkdata[selection]
     print sinkdata
-        
-    time = sinkdata['col1']
-    npart_acc = sinkdata['col2']
-    r_sink_phys = sinkdata['col3']
-    part_internal_energy = sinkdata['col4']
-    sink_entropy = sinkdata['col5']
-    part_id = sinkdata['col6']
-    sink_id  = sinkdata['col7']
-    sink_pressure = sinkdata['col8']
-
-    print 'Time Range:', time[0], time[-1]
-
-    ### Create Plot!
-    ### All plots vs density
-    print 'Plotting...'
-    fig = pyplot.figure(1,(12,10))
-    fig.clf()
-    ax0 = fig.add_subplot(221)
-    ax1 = fig.add_subplot(222)
-    ax2 = fig.add_subplot(223)
-    ax3 = fig.add_subplot(224)
-    
-    xlims = (0.03825409, 0.03825415)
-
-    # Particles accreted
-    ax0.plot(time, npart_acc)
-    #ax0.set_xscale('log')
-    #ax0.set_yscale('log')
-
-    #ax0.set_xlim(xlims)
-    #ax0.set_ylim(7, 1e4)
-    ax0.set_xlabel('Time')
-    ax0.set_ylabel('Particles Accreted')
-
-    # Sink Physical Radius
-    ax1.plot(time, r_sink_phys)
-    #ax1.set_xscale('log')
-    #ax1.set_yscale('log')
-
-    #ax1.set_xlim(xlims)
-    #ax1.set_ylim(1e-12, 1e-2)
-    ax1.set_xlabel('Time')
-    ax1.set_ylabel('Sink Radius [kpc]')
-
-    # Sink Entropy
-    ax2.plot(time, sink_entropy)
-    #ax2.set_xscale('log')
-    #ax2.set_yscale('log')
-
-    #ax2.set_xlim(xlims)
-    #ax2.set_ylim(1e-7,1)
-    ax2.set_xlabel('Time')
-    ax2.set_ylabel('Sink Entropy')
-
-    # Sink Pressure
-    ax3.plot(time, sink_pressure)
-    ax3.set_xscale('log')
-
-    #ax3.set_xlim(xlims)
-    #ax3.set_ylim(1,2)
-    ax3.set_xlabel('Time')
-    ax3.set_ylabel('Sink Pressure')
-
-    #title = fig.suptitle('Redshift: %.3f' %(redshift,))
-    fig.subplots_adjust(top=0.94, left=0.085, right=.915)
-    pyplot.savefig(write_dir+'sinkdat.png', 
-                   #bbox_extra_artists=(title,),
-                   bbox_inches='tight')
-#===============================================================================
-
-if __name__ == '__main__':
-    #pyplot.ioff()
-    wdir = os.getenv('HOME')+'/data/simplots/vanilla-100/'
-    path = os.getenv('HOME')+'/research/work/v100/sinkdat'
-    plot_sinkdat(path, wdir)
+        '''
