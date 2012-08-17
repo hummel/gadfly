8e1fb2b9ebc7edf13112b9e0d338d473f9e800e6
Jacob Hummel <jhummel@astro.as.utexas.edu>
2012-08-17 15:20:06 -0500

    SingleSink class extended to allow choosing sink by formation order. 	modified:   sinks.py

--- sinks.py
+++ sinks.py
@@ -3,6 +3,7 @@
 """
 Classes and routines for analyzing sink data output by gadget.
 """
+import warnings
 import numpy as np
 import asciitable
 #===============================================================================
@@ -26,25 +27,47 @@ class SinkData(object):
         self.sink_pressure = sinkdata['col8']
 
 class SingleSink(SinkData):
-    def __init__(self, filename, nform=None, ID=None):
-        super(SingleSink,self).__init__(filename)
-        if id:
-            # Restrict to a single sink
-            lines = np.where(self.sink_id == ID)
-            for key in vars(self).keys():
-                vars(self)[key] = vars(self)[key][lines]
+    '''
+    Select sink data for a single sink particle.
 
-            # Select final output for each timestep
-            tsteps = np.unique(self.time)
-            selection = []
-            for t in tsteps:
-                times = np.where(self.time == t)[0]
-                selection.append(times[-1])
-            for key in vars(self).keys():
-                vars(self)[key] = vars(self)[key][selection]
-        '''
-        if number <= self.unique_id.size:
-            self.sink_num = number
+    possible options:
+    nform: select the n(th) sink to form.
+    ID: select sink by ID.
+    '''
+    def __init__(self, filename, nform=None, id_=None):
+        super(SingleSink,self).__init__(filename)
+        if((nform is None) and (id_ is None)):
+            print "No sink specified: Selecting first sink to form..."
+            nform = 1
+        if nform:
+            print "Key set: nform =", nform
+            if id_: warnings.warn("nform key overrides id_")
+            # Select n(th) sink to form
+            new = []
+            i = 0
+            while len(new) < nform:
+                if self.sink_id[i] not in new:
+                    new.append(self.sink_id[i])
+                i += 1
+            print "Unique ID's found:", new
+            id_ = new[-1]
+        elif id_ is not None:
+            print "Key set: id_ =", id_
         else:
-            raise KeyError
-        '''
+            raise RuntimeError("Execution should not have reached this point!")
+        print "Using sink ID", id_
+        
+        # Restrict to a single sink
+        lines = np.where(self.sink_id == id_)
+        for key in vars(self).keys():
+            vars(self)[key] = vars(self)[key][lines]
+
+        # Select final output for each timestep
+        tsteps = np.unique(self.time)
+        selection = []
+        for t in tsteps:
+            times = np.where(self.time == t)[0]
+            selection.append(times[-1])
+        for key in vars(self).keys():
+                vars(self)[key] = vars(self)[key][selection]
+        self.id_ = id_
