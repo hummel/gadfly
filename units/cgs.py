ab5a04a82b6d5f883396aa3f286690ec84da6378
Jacob Hummel <jhummel@astro.as.utexas.edu>
2012-08-20 10:37:32 -0500

    Added extra time conversions to cgs. 	modified:   units/cgs.py

--- units/cgs.py
+++ units/cgs.py
@@ -6,8 +6,13 @@ Physical cgs unit conversions for analyzing my Gadget2 HDF5 snapshot data.
 Mass_g = 1.989e43
 Length_cm = 3.085678e21
 Velocity_cgs= 1.0e5
-Density_cgs= Mass_g / Length_cm**3
+
 Time_s= Length_cm / Velocity_cgs
 Density_cgs= Mass_g / Length_cm**3
 Pressure_cgs= Mass_g / Length_cm/ Time_s**2
 Energy_cgs= Mass_g * Length_cm**2 / Time_s**2
+
+Time_yr = Time_s/3.15569e7
+Time_myr = Time_yr/1e6
+Time_gyr = Time_yr/1e9
+
