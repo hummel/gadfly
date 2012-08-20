02f7c4058fe65a6baca96a793031fc8e371d7dad
Jacob Hummel <jhummel@astro.as.utexas.edu>
2012-08-20 16:34:27 -0500

    Added Methods for returning standard particle data to class PartTypeX.

--- gadgetHDF5.py
+++ gadgetHDF5.py
@@ -5,6 +5,7 @@ This module contains classes for reading Gadget2 HDF5 snapshot data.
 """
 
 import h5py
+import units
 
 class Snapshot:
     """
@@ -29,10 +30,16 @@ class HDF5Group:
     Base Class for HDF5 groups
     """
     def keys(self):
+        """
+        Print keys for all arrays in the hdf5 group.
+        """
         for key in vars(self):
             print key
 
     def get(self,key):
+        """
+        Return the raw values of the array from the HDF5 group.
+        """
         return vars(self)[key].value
         
 class Header(HDF5Group):
@@ -54,5 +61,29 @@ class PartTypeX(HDF5Group):
             key = item[0].replace(' ', '_')
             vars(self)[key] = item[1]
 
-        
+    def getMass(self, conv=units.Mass_sun):
+        """
+        Default: Return Particle Masses in units of M_sun/h
+        conv: use the given unit conversion from code units
+        """
+        return self.Masses.value*conv
 
+    def getCoords(self, conv=units.Length_kpc):
+        """
+        Default: Return Particle Coordinates in units of kpc/h
+        conv: use the given unit conversion from code units
+        """
+        return self.Coordinates.value*conv
+
+    def getVelocities(self, conv=units.Velocity_km_s):
+        """
+        Default: Return Particle Velocities in units of km/s
+        conv: use the given unit conversion from code units
+        """
+        return self.Velocities.value*conv
+
+    def getPIDs(self):
+        """
+        Return Particle ID numbers
+        """
+        return self.ParticleIDs.value
