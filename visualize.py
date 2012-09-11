182678bda31acd6c4463c6069ad7afde68fa8464
Jacob Hummel <jhummel@astro.as.utexas.edu>
2012-09-11 13:14:54 -0500

    Memory optimizations for parallel performance. 	modified:   visualize.py

--- visualize.py
+++ visualize.py
@@ -12,30 +12,31 @@ import pp
 
 import pyGadget
 #===============================================================================
-def density_map(pps,width, x,y,dens,hsml,zshape):
+def scalar_map(pps,width, x,y,scalar_field,hsml,zshape):
     zi = numpy.zeros(zshape)
     nzi = numpy.zeros_like(zi)
     i_min = (x - hsml + width/2.0) / width*pps
     i_max = (x + hsml + width/2.0) / width*pps
     j_min = (y - hsml + width/2.0) / width*pps
     j_max = (y + hsml + width/2.0) / width*pps
-    weight = dens*dens
-    for i in range(int(pps)):
-        if(i >= i_min and i <= i_max):
-            center_i = -width/2.0 + (i+0.5) * width/pps
-            for j in range(int(pps)):
-                if(j >= j_min and j <= j_max):
-                    center_j = -width/2.0 + (j+0.5) * width/pps
-                    r2 = ((x - center_i)**2
-                          + (y - center_j)**2) / hsml / hsml
-                    if(r2 <= 1.0):
-                        r = numpy.sqrt(r2)
-                        if(r <= 0.5):
-                            W_x = 1.0 - 6.0 * r**2 + 6.0 * r**3
-                        else:
-                            W_x = 2.0 * (1.0 - r)**3
-                        zi[i][j] += weight * dens * W_x
-                        nzi[i][j] += weight * W_x
+    weight = scalar_field*scalar_field
+    for n in range(scalar_field.size):
+        for i in xrange(pps):
+            if(i >= i_min[n] and i <= i_max[n]):
+                center_i = -width/2.0 + (i+0.5) * width/pps
+                for j in xrange(pps):
+                    if(j >= j_min[n] and j <= j_max[n]):
+                        center_j = -width/2.0 + (j+0.5) * width/pps
+                        r2 = ((x[n] - center_i)**2
+                              + (y[n] - center_j)**2) / hsml[n] / hsml[n]
+                        if(r2 <= 1.0):
+                            r = numpy.sqrt(r2)
+                            if(r <= 0.5):
+                                W_x = 1.0 - 6.0 * r**2 + 6.0 * r**3
+                            else:
+                                W_x = 2.0 * (1.0 - r)**3
+                            zi[i][j] += weight[n] * scalar_field[n] * W_x
+                            nzi[i][j] += weight[n] * W_x
     return zi,nzi
 #===============================================================================
 
@@ -44,7 +45,9 @@ pps = 800 # 'pixels' per side
 hsml_factor = 1.7
 
 write_dir = os.getenv('HOME')+'/data/simplots/vanilla-100/'
-for snap in range(467,468):
+for snap in xrange(467,468):
+    #Create jobserver
+    job_server = pp.Server(ppservers=("*",))
     path = (os.getenv('HOME')+'/sim/vanilla-100/snapshot_'+
             '{:0>3}'.format(snap)+'.hdf5')
 
@@ -82,7 +85,7 @@ for snap in range(467,468):
     #pos = pos[refined]
     print 'Refinement complete.'
 
-    width= 3e-2
+    width= 1e0
     depth= width/100
     center = pos[dens.argmax()]
     x = pos[:,0] - center[0]
@@ -125,26 +128,38 @@ for snap in range(467,468):
     zi = numpy.zeros_like(xi)
     nzi = numpy.zeros_like(zi)
     zshape = zi.shape
-    print 'Mesh created.'
 
-
-#===============================================================================
     hsml = numpy.fmax(hsml_factor * smL, width / pps / 2.0)
+    # if sink smoothing link is too inflated, 
+    # artificially set it to accretion radius value
+    #if(sinkval[n] > 0):
+    #    print 'sinkval > 0 !!!'
+    #    hsml[n] = hsml_factor * 3.57101e-07
 
-    job_server = pp.Server(ppservers=("*",))
-    jobs = []
     print 'Distributing...'
-    for n in range(dens.size):
-        # if sink smoothing link is too inflated, 
-        # artificially set it to accretion radius value
-        if(sinkval[n] > 0):
-            print 'sinkval > 0 !!!'
-            hsml[n] = hsml_factor * 3.57101e-07
-#        zi,nzi = density_map(pps,width, x[n],y[n],dens[n],hsml[n],zi)
-        jobs.append(job_server.submit(density_map,
-                                      (pps,width, x[n],y[n],dens[n],
-                                       hsml[n],zshape),
+    jobs = []
+    server_list = job_server.get_active_nodes()
+    ncpus = sum(server_list.values())
+    #Divide in to 8X as many tasks as there are cpus.
+    parts = ncpus*8
+    start = 0
+    end = dens.size - 1
+    step = (end - start) / parts + 1
+    print 'Dividing the input into',parts,'arrays of',step,'particles'
+    print 'for calculation on',ncpus,'cpus.'
+    for cpu in xrange(parts):
+        nstart = start+cpu*step
+        nend = min(start+(cpu+1)*step, end)
+        #'''
+        jobs.append(job_server.submit(scalar_map,
+                                      (pps,width, 
+                                       x[nstart:nend],
+                                       y[nstart:nend],
+                                       dens[nstart:nend],
+                                       hsml[nstart:nend],zshape),
                                       (),('numpy',)))
+        #'''
+    #sys.exit()
     print 'Calculating...'
     for job in jobs:
         pzi,pnzi = job()
@@ -152,6 +167,7 @@ for snap in range(467,468):
         nzi += pnzi
 
     job_server.print_stats()
+    job_server.destroy()
     zi = numpy.where(nzi > 0, zi/nzi, zi)
     #zi = numpy.fmax(zi, zmin)
     #zi = numpy.fmin(zi, zmax)
