051ebcd361c3b156c155eb8e57b4bd9d3e9744e2
Jacob Hummel <jhummel@astro.as.utexas.edu>
2012-09-25 09:32:17 -0500

    Modified particles.py to plot just those particles right around the sink. 	modified:   particles3D.py

--- particles3D.py
+++ particles3D.py
@@ -17,7 +17,7 @@ import zoomed
 import directory
 
 #===============================================================================
-def plotgas(ax, path, mmh_com=(50,50,50), stride=50):
+def plotgas(ax, path, mmh_com=(50,50,50), stride=1):
     snap = path[-8:-5]
     f = h5py.File(path,'r')
     
@@ -43,6 +43,7 @@ def plotgas(ax, path, mmh_com=(50,50,50), stride=50):
     boxSize = header.attrs.get('BoxSize')
     h = header.attrs.get("HubbleParam") # H = 100*h
     redshift = header.attrs.get('Redshift')
+    a = 1/(1+redshift)
     f.close()
 
     pos = partTypeN_readHDF5(path, 'Coordinates', 'gas')
@@ -64,12 +65,13 @@ def plotgas(ax, path, mmh_com=(50,50,50), stride=50):
     abundances = abundances[refined]
 
     # Unit Conversions
-    boxSize = boxSize/h     
-    pos = pos/h             
-    mmh_com = mmh_com/h     
+    boxSize = boxSize/h*a
+    pos = pos/h*a
+    mmh_com = mmh_com/h*a
 
     dens = dens * unitDensity_cgs * h*h * (1 + redshift)**3
     dens = dens * X_h / m_H
+    print 'Max Density: %.5e' %dens.max()
     energy = energy * unitEnergy_cgs / unitMass_g
 
     # 3D positions
@@ -89,55 +91,39 @@ def plotgas(ax, path, mmh_com=(50,50,50), stride=50):
     z = numpy.where(z>L,(z-boxSize),z)
     z = numpy.where(z<-L,(z+boxSize),z)
 
-    # select only central particles
-    r =  numpy.sqrt(x*x + y*y + z*z)
-    inR = numpy.where(r <= 10)
-    
-    x = x[inR]
-    y = y[inR]
-    z = z[inR]
-    particle_mass = particle_mass[inR]
-    dens = dens[inR]
-    energy = energy[inR]
-    gamma = gamma[inR]
-    abundances = abundances[inR]
+    # Change units from kpc to pc
+    x = x*1e3
+    y = y*1e3
+    z = z*1e3
 
     # Split in to most refined level and less refined
-    refined = numpy.where(particle_mass <= minimum)[0]
-    particle_mass0 = particle_mass[refined]
-    dens0 = dens[refined]
-    energy0 = energy[refined]
-    gamma0 = gamma[refined]
-    abundances0 = abundances[refined]
-    x0 = x[refined]
-    y0 = y[refined]
-    z0 = z[refined]
-
-    refined = numpy.where(particle_mass > minimum)[0]
-    particle_mass1 = particle_mass[refined]
-    dens1 = dens[refined]
-    energy1 = energy[refined]
-    gamma1 = gamma[refined]
-    abundances1 = abundances[refined]
-    x1 = x[refined]
-    y1 = y[refined]
-    z1 = z[refined]
+    refined = numpy.where(dens >= 7e9)[0]
+    print refined.size, 'particles found'
+    particle_mass = particle_mass[refined]
+    dens = dens[refined]
+    energy = energy[refined]
+    gamma = gamma[refined]
+    abundances = abundances[refined]
+    x = x[refined]
+    y = y[refined]
+    z = z[refined]
+    print ' x:: max: %.6e min: %.6e' %(x.max(),x.min())
+    print ' y:: max: %.6e min: %.6e' %(y.max(),y.min())
+    print ' z:: max: %.6e min: %.6e' %(z.max(),z.min())
 
     # Chemical Abundances
     # 0:H2I 1:HII 2:DII 3:HDI 4:HeII 5:HeIII
-    H2I = abundances0[:,0]
+    H2I = abundances[:,0]
     h2frac = 2*H2I
     mu = (0.24/4.0) + ((1.0-h2frac)*0.76) + (h2frac*.76/2.0)
     mu = 1 / mu # mean molecular weight
 
     # Derived Properties
-    temp = (mu * m_H / k_B) * energy0 * (gamma0-1)
-    hot = numpy.where(temp > 8e2*dens0**.5)[0]
+    temp = (mu * m_H / k_B) * energy * (gamma-1)
+    hot = numpy.where(temp > 8e2*dens**.5)[0]
 
     print 'Plotting...'
-    ax.scatter3D(x0, y0, z0, s=.5, c='k', linewidths=0.0)
-    ax.scatter3D(x0[hot], y0[hot], z0[hot], s=5, c='r', linewidths=0.0)
-    ax.scatter3D(x1, y1, z1, s=8, c='b', linewidths=0.0)
+    ax.scatter3D(x,y,z, s=10)
     return ax, redshift
 
 #===============================================================================
@@ -182,9 +168,9 @@ def plotdm(ax, path, mmh_com=(50,50,50), stride=50):
     particle_mass = particle_mass[refined]
 
     # Unit Conversions
-    boxSize = boxSize/h     
-    pos = pos/h             
-    mmh_com = mmh_com/h     
+    boxSize = boxSize/h
+    pos = pos/h
+    mmh_com = mmh_com/h
 
     # Initialization Complete --- Begin Analysis
     x = pos[:,0]
@@ -205,6 +191,7 @@ def plotdm(ax, path, mmh_com=(50,50,50), stride=50):
 
     print 'Plotting...'
     ax.scatter3D(x, y, z, s=.5, c='k', linewidths=0.0)
+    pyplot.draw()
     return ax, redshift
 
 #===============================================================================
@@ -220,24 +207,26 @@ def uniplot(path, write_dir, ptype, stride=50):
         ax,redshift = plotdm(ax, path, stride=stride)
     else:
         raise ValueError("ptype must be either 'dm' or 'gas'")
-    ax.set_xlim3d(-10,10)
-    ax.set_ylim3d(-10,10)
-    ax.set_zlim3d(-10,10)
+    ax.set_xlim3d(17.318,17.321)
+    ax.set_ylim3d(13.9495,13.9525)
+    ax.set_zlim3d(-20.305,-20.302)
 
     pyplot.title('Redshift: %.2f' %(redshift,))
-    ax.set_xlabel('[comoving kpc]')
-    ax.set_ylabel('[comoving kpc]')
-    ax.set_zlabel('[comoving kpc]')
+    ax.set_xlabel('[physical pc]')
+    ax.set_ylabel('[physical pc]')
+    ax.set_zlabel('[physical pc]')
+    #ax.mouse_init()
+    pyplot.draw()
     pyplot.savefig(write_dir+snap+'-'+ptype+'.png',
                    bbox_inches='tight')
-    #ax.mouse_init()
+
 #===============================================================================
 
 if __name__ == '__main__':
-    pyplot.ioff()
+    #pyplot.ioff()
     wdir = os.getenv('HOME')+'/data/simplots/vanilla/'
-    for snap in range(224,227): 
+    for snap in xrange(280,300): 
         path = (os.getenv('HOME')+'/sim/vanilla/snapshot_'+
                 '{:0>3}'.format(snap)+'.hdf5')
-        uniplot(path, wdir, 'gas', stride=50)
-        uniplot(path, wdir, 'dm', stride=50)
+        uniplot(path, wdir, 'gas', stride=1)
+        #uniplot(path, wdir, 'dm', stride=50)
