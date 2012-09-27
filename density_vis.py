17bb2a9853e46f24a681fa4f927216daea5dd8cd
Jacob Hummel <jhummel@astro.as.utexas.edu>
2012-09-27 14:14:31 -0500

    Made density_vis.py plot visualization from all 3 cardinal directions. 	modified:   density_vis.py

--- density_vis.py
+++ density_vis.py
@@ -20,34 +20,35 @@ import statistics
 length_unit = units.Length_AU
 pps = 1000 # 'pixels' per side
 hsml_factor = 1.7
-path = os.getenv('HOME')+'/sim/vanilla/snapshot_'
-write_dir = os.getenv('HOME')+'/data/simplots/vanilla/'
-suffix = '-dens-xy.png'
-view = suffix[-6:-4]
+#path = os.getenv('HOME')+'/sim/vanilla/snapshot_'
+#write_dir = os.getenv('HOME')+'/data/simplots/vanilla/'
+path = os.getenv('HOME')+'/sim/test/snapshot_'
+write_dir = os.getenv('HOME')+'/data/simplots/test/'
 boxsize = 5e3
+
 #pyplot.ioff()
-
 job_server = pp.Server()
-for i in [600]:
-#for i in xrange(468,614):
-    fname = path + '{:0>3}'.format(i) + '.hdf5'
-    wpath = write_dir + '{:0>3}'.format(i) + suffix
 
-    snap = snapshot.load(fname)
-    redshift = snap.header.Redshift
-    x,y,z = visualize.density(snap, view, boxsize, 1., length_unit, 
-                              job_server, pps, hsml_factor)
-    snap.close()
-    print 'Plotting...'
-    fig = pyplot.figure(1,(10,10))
-    fig.clf()
-    pyplot.imshow(z, extent=[x.min(),x.max(),y.min(),y.max()])
-    #pyplot.colorbar()
-    ax = pyplot.gca()
-    ax.set_xlabel('AU')
-    ax.set_ylabel('AU')
-    ax.set_title('Redshift: %.5f' %redshift)
-    pyplot.draw()
-    #pyplot.savefig(wpath, 
-    #               bbox_inches='tight')
-    #pyplot.show()
+for suffix in ['-dens-xy.png','-dens-xz.png','-dens-yz.png']:
+    view = suffix[-6:-4]
+    for i in xrange(560,690):
+        fname = path + '{:0>3}'.format(i) + '.hdf5'
+        wpath = write_dir + '{:0>3}'.format(i) + suffix
+
+        snap = snapshot.load(fname)
+        redshift = snap.header.Redshift
+        x,y,z = visualize.density(snap, view, boxsize, 1., length_unit, 
+                                  job_server, pps, hsml_factor)
+        snap.close()
+        print 'Plotting',view+'...'
+        fig = pyplot.figure(1,(10,10))
+        fig.clf()
+        pyplot.imshow(z, extent=[x.min(),x.max(),y.min(),y.max()])
+        #pyplot.colorbar()
+        ax = pyplot.gca()
+        ax.set_xlabel('AU')
+        ax.set_ylabel('AU')
+        ax.set_title('Redshift: %.5f' %redshift)
+        pyplot.draw()
+        pyplot.savefig(wpath, 
+                       bbox_inches='tight')
