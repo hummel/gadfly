fcaca606cb56af87b2ff676c6004f4a213d5910c
Jacob Hummel <jhummel@astro.as.utexas.edu>
2012-10-10 10:59:21 -0500

    Added overplotting of sink position; reorganized to improve IO performance. 	modified:   density_vis.py

--- density_vis.py
+++ density_vis.py
@@ -19,36 +19,46 @@ import statistics
 
 length_unit = units.Length_AU
 pps = 1000 # 'pixels' per side
-hsml_factor = 1.7
+hsml_factor = .8
 #path = os.getenv('HOME')+'/sim/vanilla/snapshot_'
 #write_dir = os.getenv('HOME')+'/data/simplots/vanilla/'
 path = os.getenv('HOME')+'/sim/test/snapshot_'
 write_dir = os.getenv('HOME')+'/data/simplots/test/'
-boxsize = 5e3
+boxsize = 6e2
 
-#pyplot.ioff()
+pyplot.ioff()
 job_server = pp.Server()
 
-for suffix in ['-dens-xy.png','-dens-xz.png','-dens-yz.png']:
-    view = suffix[-6:-4]
-    for i in xrange(560,690):
-        fname = path + '{:0>3}'.format(i) + '.hdf5'
+for i in xrange(300,841):
+    fname = path + '{:0>3}'.format(i) + '.hdf5'
+    snap = snapshot.load(fname)
+    redshift = snap.header.Redshift
+    for suffix in ['-dens-xy.png','-dens-xz.png','-dens-yz.png']:
         wpath = write_dir + '{:0>3}'.format(i) + suffix
-
-        snap = snapshot.load(fname)
-        redshift = snap.header.Redshift
+        view = suffix[-6:-4]
         x,y,z = visualize.density(snap, view, boxsize, 1., length_unit, 
                                   job_server, pps, hsml_factor)
-        snap.close()
         print 'Plotting',view+'...'
         fig = pyplot.figure(1,(10,10))
         fig.clf()
         pyplot.imshow(z, extent=[x.min(),x.max(),y.min(),y.max()])
         #pyplot.colorbar()
         ax = pyplot.gca()
+        for sink in snap.sinks:
+            if view == 'xy':
+                ax.plot(sink[0],sink[1],'ko')
+            elif view == 'xz':
+                ax.plot(sink[0],sink[2],'ko')
+            elif view == 'yz':
+                ax.plot(sink[1],sink[2],'ko')
+            else:
+                raise KeyError
         ax.set_xlabel('AU')
         ax.set_ylabel('AU')
         ax.set_title('Redshift: %.5f' %redshift)
         pyplot.draw()
         pyplot.savefig(wpath, 
                        bbox_inches='tight')
+    snap.close()
+job_server.destroy()
+
