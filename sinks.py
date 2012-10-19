1df89a400671b0b563dd8dc1b2fa16e2e5223fbd
Jacob Hummel <jhummel@astro.as.utexas.edu>
2012-10-19 12:48:16 -0500

    Made directory a command line argument. 	modified:   sinks.py

--- sinks.py
+++ sinks.py
@@ -3,55 +3,53 @@
 # Jacob Hummel
 
 import os
+import sys
 from matplotlib import pyplot
 
 import pyGadget
-from pyGadget import units
 #===============================================================================
 
-def plot_sinkdat(path, write_dir):
-    
-    sink = pyGadget.sinks.SingleSink(path,1)
-    sink.time = sink.time - sink.time[0] # set t=0 at sink formation.
+if len(sys.argv) < 2:
+    print 'Usage: python sinks.py (simulation name)'
+    sys.exit()
 
-    ### Create Plot!
-    print 'Plotting...'
-    fig = pyplot.figure(1,(12,10))
-    fig.clf()
-    ax0 = fig.add_subplot(221)
-    ax1 = fig.add_subplot(222)
-    ax2 = fig.add_subplot(223)
-    ax3 = fig.add_subplot(224)
-    
-    # Particles accreted
-    ax0.plot(sink.time, sink.npart_acc,'o')
-    ax0.set_xlabel('Time Since Formation [yr]')
-    ax0.set_ylabel('Particles Accreted')
+simulation = sys.argv[1]
+path = os.getenv('HOME')+'/data/sinks/'+simulation+'/sinkdat'
+wdir = os.getenv('HOME')+'/data/sinks/'+simulation+'/'
+sink = pyGadget.sinks.SingleSink(path,1)
+sink.time = sink.time - sink.time[0] # set t=0 at sink formation.
 
-    # Sink Physical Radius
-    ax1.plot(sink.time, sink.radius)
-    ax1.set_xlabel('Time Since Formation [yr]')
-    ax1.set_ylabel('Sink Radius [AU/h]')
+### Create Plot!
+print 'Plotting...'
+fig = pyplot.figure(1,(12,10))
+fig.clf()
+ax0 = fig.add_subplot(221)
+ax1 = fig.add_subplot(222)
+ax2 = fig.add_subplot(223)
+ax3 = fig.add_subplot(224)
 
-    # Sink Entropy
-    ax2.plot(sink.time, sink.entropy)
-    ax2.set_xlabel('Time Since Formation [yr]')
-    ax2.set_ylabel('Sink Entropy [???]')
+# Particles accreted
+ax0.plot(sink.time, sink.npart_acc,'o')
+ax0.set_xlabel('Time Since Formation [yr]')
+ax0.set_ylabel('Particles Accreted')
 
-    # Sink Pressure
-    ax3.plot(sink.time, sink.pressure)
-    ax3.set_xlabel('Time Since Formation [yr]')
-    ax3.set_ylabel('Sink Pressure [dyne cm$^{-2}$]')
-    #ax3.set_yscale('log')
-    ax3.set_ylim(.1,.5)
+# Sink Physical Radius
+ax1.plot(sink.time, sink.radius)
+ax1.set_xlabel('Time Since Formation [yr]')
+ax1.set_ylabel('Sink Radius [AU/h]')
 
-    fig.subplots_adjust(top=0.94, left=0.1, right=.915)
-    pyplot.savefig(write_dir+'sinkdat.png', 
-                   bbox_inches='tight')
-#===============================================================================
+# Sink Entropy
+ax2.plot(sink.time, sink.entropy)
+ax2.set_xlabel('Time Since Formation [yr]')
+ax2.set_ylabel('Sink Entropy [???]')
 
-if __name__ == '__main__':
-    #pyplot.ioff()
-    wdir = os.getenv('HOME')+'/data/simplots/test/'
-    path = os.getenv('HOME')+'/work/test/sinkdat'
-    plot_sinkdat(path, wdir)
+# Sink Pressure
+ax3.plot(sink.time, sink.pressure)
+ax3.set_xlabel('Time Since Formation [yr]')
+ax3.set_ylabel('Sink Pressure [dyne cm$^{-2}$]')
+#ax3.set_yscale('log')
+ax3.set_ylim(.1,.5)
+
+fig.subplots_adjust(top=0.94, left=0.1, right=.915)
+pyplot.savefig(wdir+'sinkdat.png', 
+               bbox_inches='tight')
