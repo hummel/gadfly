77690302f16e3c893e3a41b7afeebed16ac3a697
Jacob Hummel <jhummel@astro.as.utexas.edu>
2012-10-25 15:16:54 -0500

    Modularized script for easier parallelization; allow for multiple usage options via sys.argv 	modified:   gas_temp.py

--- gas_temp.py
+++ gas_temp.py
@@ -3,20 +3,28 @@
 
 import os
 import sys
+import glob
 import numpy
 from matplotlib import pyplot
 import pyGadget
 #===============================================================================
 
-def plot_temp(path, write_dir):
+def load_snapshot(path):
     snapshot = pyGadget.snapshot.load(path)
-    h = snapshot.header.HubbleParam # H = 100*h
+    snapshot.gas.load_masses()
+    snapshot.gas.load_number_density()
+    snapshot.gas.load_internal_energy()
+    snapshot.gas.load_gamma()
+    snapshot.gas.load_H2_fraction()
+    snapshot.close()
+    return snapshot
+
+def plot_temp(snapshot):
+    h = snapshot.header.HubbleParam
     redshift = snapshot.header.Redshift
     particle_mass = snapshot.gas.get_masses()
     dens = snapshot.gas.get_number_density()
-    h2frac = snapshot.gas.get_H2_fraction()
     temp = snapshot.gas.get_temperature()
-    snapshot.close()
 
     # Refine
     print 'Refining...'
@@ -24,12 +32,11 @@ def plot_temp(path, write_dir):
     stride = 100
     refined = numpy.where(particle_mass <= minimum)[0]#[0:-1:stride]
     dens = dens[refined]
-    h2frac = h2frac[refined]
     temp = temp[refined]
 
     # Plot!
     print 'Plotting...'
-    fig = pyplot.figure(1,(15,10))
+    fig = pyplot.figure(figsize=(15,10))
     fig.clf()
     ax = fig.add_subplot(111)
     ax.scatter(dens, temp, s=.75, c='k', linewidths=0.0)
@@ -41,25 +48,55 @@ def plot_temp(path, write_dir):
     pyplot.title('Redshift: %.2f' %(redshift,))
     ax.set_xlabel('n [cm^-3]')
     ax.set_ylabel('Temperature [K]')
-    snap = path[-8:-5]
-    pyplot.savefig(write_dir+snap+'-temp.png',
+    return fig
+
+def save_plot(fig,path):
+    pyplot.savefig(path+'-temp.png',
                    bbox_inches='tight')
+#===============================================================================
+def multitask(path,write_dir,start,stop):
+    for snap in xrange(start,stop):
+        fname = path + '{:0>3}'.format(snap)+'.hdf5'
+        print 'loading', fname
+        snapshot = load_snapshot(fname)
+        fig = plot_temp(snapshot)
+        save_plot(fig,write_dir+str(snapshot.number))
+
 #===============================================================================
 
 if __name__ == '__main__':
     pyplot.ioff()
-    if len(sys.argv) < 4:
-        print 'Usage: python gas_temp.py [simulation name] '\
+    if ((len(sys.argv) not in [2,3,4]) or (sys.argv[1] == '-h')):
+        print 'Usage::'
+        print '   Option 1: python gas_temp.py [simulation name] '\
             '[beginning snapshot] [final snapshot]'
+        print '   Option 2: python gas_temp.py [simulation name] '\
+            '[single snapshot]'
+        print '   Option 3: python gas_temp.py [simulation name] '\
+            '(creates a plot for every snapshot)'
         sys.exit()
 
     simulation = sys.argv[1]
     path = os.getenv('HOME')+'/sim/'+simulation+'/snapshot_'
     write_dir = os.getenv('HOME')+'/data/simplots/'+simulation+'/'
 
-    start = int(sys.argv[2])
-    stop = int(sys.argv[3])+1
-    for snap in xrange(start,stop):
+    if len(sys.argv) == 3:
+        snap = sys.argv[2]
         fname = path + '{:0>3}'.format(snap)+'.hdf5'
         print 'loading', fname
-        plot_temp(fname, write_dir)
+        snapshot = load_snapshot(fname)
+        fig = plot_temp(snapshot)
+        save_plot(fig,write_dir+snap)
+        
+    elif len(sys.argv) == 4:
+        start = int(sys.argv[2])
+        stop = int(sys.argv[3])+1
+        multitask(path,write_dir,start,stop)
+
+    else:
+        files = glob.glob(path+'???.hdf5')
+        files.sort()
+        start = int(files[0][-8:-5])
+        stop = start + len(files)
+        multitask(path,write_dir,start,stop)
+
