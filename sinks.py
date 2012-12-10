ff1057a4ffc5b96888dffe22d0df9c768fbc8adf
Jacob Hummel <jhummel@astro.as.utexas.edu>
2012-12-10 14:14:32 -0600

    Plot sink mass instead of entropy, and plot multiple sinks in one figure. 	modified:   sinks.py

--- sinks.py
+++ sinks.py
@@ -14,10 +14,13 @@ if len(sys.argv) < 2:
     sys.exit()
 
 simulation = sys.argv[1]
-path = os.getenv('HOME')+'/data/sinks/'+simulation+'/sinkdat'
+path = os.getenv('HOME')+'/data/sinks/'+simulation+'/'
 wdir = os.getenv('HOME')+'/data/sinks/'+simulation+'/'
-sink = pyGadget.sinks.SingleSink(path,1)
-sink.time = sink.time - sink.time[0] # set t=0 at sink formation.
+sink1 = pyGadget.sinks.SingleSink(path,1)
+sink2 = pyGadget.sinks.SingleSink(path,2)
+t0 = sink1.time[0] # set t=0 at first sink formation.
+sink1.time = sink1.time - t0
+sink2.time = sink2.time - t0
 
 ### Create Plot!
 print 'Plotting...'
@@ -29,27 +32,35 @@ ax2 = fig.add_subplot(223)
 ax3 = fig.add_subplot(224)
 
 # Particles accreted
-ax0.plot(sink.time, sink.npart_acc,'o')
+ax0.plot(sink1.time, sink1.npart_acc,'o',label='Sink 1')
+ax0.plot(sink2.time, sink2.npart_acc,'o',label='Sink 2')
 ax0.set_xlabel('Time Since Formation [yr]')
 ax0.set_ylabel('Particles Accreted')
+ax0.legend()
 
 # Sink Physical Radius
-ax1.plot(sink.time, sink.radius)
+ax1.plot(sink1.time, sink1.radius)
+ax1.plot(sink2.time, sink2.radius)
 ax1.set_xlabel('Time Since Formation [yr]')
-ax1.set_ylabel('Sink Radius [AU/h]')
+ax1.set_ylabel('Sink Radius [AU]')
 
-# Sink Entropy
-ax2.plot(sink.time, sink.entropy)
+# Sink Mass
+ax2.plot(sink1.time, sink1.mass)
+ax2.plot(sink2.time, sink2.mass)
 ax2.set_xlabel('Time Since Formation [yr]')
-ax2.set_ylabel('Sink Entropy [???]')
+ax2.set_ylabel('Sink Mass [M$_{\odot}$]')
 
 # Sink Pressure
-ax3.plot(sink.time, sink.pressure)
+ax3.plot(sink1.time, sink1.pressure)
+ax3.plot(sink2.time, sink2.pressure)
 ax3.set_xlabel('Time Since Formation [yr]')
 ax3.set_ylabel('Sink Pressure [dyne cm$^{-2}$]')
 #ax3.set_yscale('log')
 ax3.set_ylim(.1,.5)
 
+z = 1/sink1.a[0] -1
+fig.suptitle('First Sink Formation: z = %.2f' %z)
 fig.subplots_adjust(top=0.94, left=0.1, right=.915)
-pyplot.savefig(wdir+'sinkdat.png', 
+pyplot.savefig(wdir+'sinks.png', 
                bbox_inches='tight')
+
