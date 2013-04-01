81e4041062139be3943a07af075c63397469b580
Jacob Hummel <jhummel@astro.as.utexas.edu>
2013-03-31 20:23:01 -0500

    reworked structure visualization. 	modified:   structure_vis.py

--- structure_vis.py
+++ structure_vis.py
@@ -1,273 +1,148 @@
 #!/usr/bin/env python
-# structure_vis.py
+# density_vis.py
 # Jacob Hummel
 
 import os
 import sys
+import glob
 import numpy
-
-from scipy import weave
-from scipy.weave import converters
+import Queue
 from matplotlib import pyplot
+from matplotlib import cm
 from matplotlib.mlab import griddata
-import pp
 
 import pyGadget
+import statistics
 
+global t0
 #===============================================================================
-def scalar_map(pps,width, x,y,scalar_field,hsml,zshape):
-    zi = numpy.zeros(zshape)
-    nzi = numpy.zeros_like(zi)
-    N_gas = scalar_field.size
+def load_dens(fname,length_unit):
+    global t0
+    snapshot = pyGadget.snapshot.File(fname)
+    snapshot.gas.load_masses()
+    snapshot.gas.load_number_density()
+    snapshot.gas.load_coords(length_unit)
+    smL = snapshot.gas.get_smoothing_length(length_unit)
+    sinkval = snapshot.gas.get_sinks()
 
-    code = \
-        """
-        int i,j,n, i_min,i_max,j_min,j_max;
-        int flag_i = 0;
-        int flag_j = 0;
-        double center_i,center_j;
-        double r,r2,weight,W_x;
-        for(n =0; n < N_gas; n++) 
-          {
-            i = 0;
-            j = 0;
-            i_min = int((x(n) - hsml(n) + width/2.0) / width*pps);
-            i_max = int((x(n) + hsml(n) + width/2.0) / width*pps);
-            j_min = int((y(n) - hsml(n) + width/2.0) / width*pps);
-            j_max = int((y(n) + hsml(n) + width/2.0) / width*pps);
-            weight = scalar_field(n)*scalar_field(n);
-            do 
-              {
-                if(i >= i_min && i <= i_max)
-                  {
-                    flag_i = 1;
-                    center_i = -width/2.0 + (i+0.5) * width/ (double) pps;
-                    do
-                      {
-                        if(j >= j_min && j <= j_max)
-                          {
-                            flag_j = 1;
-                            center_j = -width/2.0 + (j+0.5)
-                              * width / (double) pps;
-                            r2 = ((x(n) - center_i) * (x(n) - center_i)
-                                  + (y(n) - center_j) * (y(n) - center_j))
-                              / hsml(n) / hsml(n);
-                            if(r2 <= 1.0)
-                              {
-                                r = sqrt(r2);
-                                if(r <= 0.5)
-                                  W_x = 1.0 - 6.0 * r*r + 6.0 * r*r*r;
-                                else
-                                  W_x = 2.0 * (1.0-r) * (1.0-r) * (1.0-r);
-                                zi(i,j) += weight * scalar_field(n) * W_x;
-                                nzi(i,j) += weight * W_x;
-                              }
-                          }
-                        else if(j > j_max)
-                          {
-                            flag_j = 2;
-                          }
-                        else
-                          {
-                            flag_j = 0;
-                          }
-                        j++;
-                      }
-                    while((flag_j == 0 || flag_j == 1) && j < pps);
-                    j = 0;
-                  }
-                else if(i > i_max)
-                  {
-                    flag_i = 2;
-                  }
-                else
-                  {
-                    flag_i = 0;
-                  }
-                i++;
-              }
-            while((flag_i == 0 || flag_i == 1) && i < pps);
-            i = 0;
-          }
-        """
-    weave.inline(code,['pps','width','x','y',
-                       'scalar_field','hsml','zi','nzi','N_gas'],
-                 type_converters=converters.blitz)
-    return zi,nzi
+    ### Sinks!
+    ids = snapshot.sink_ids = numpy.where(sinkval != 0)[0] 
+    if snapshot.sink_ids.size > 0:
+        print snapshot.sink_ids.size,'sinks found.'
+        if t0 is None:
+            t0 = snapshot.header.Time*pyGadget.units.Time_yr
+        # If sink smooting length is too inflated, artificially shrink it.
+        snapshot.gas.smoothing_length[ids] *= 1e-6
 
+    return snapshot
+
+def plot_dens(snap, write_dir, boxsize, length_unit, pps, hsml_factor):
+    global t0
+    for suffix in ['-box-xy.png']:
+        wpath = write_dir + '{:0>4}'.format(snap.number) + suffix
+        view = suffix[-6:-4]
+        boxsize = boxsize/snap.header.Redshift
+        x,y,z = pyGadget.visualize.density(snap, view, boxsize, 1., 
+                                           length_unit, t0, pps, hsml_factor)
+        z = numpy.log10(z)
+        zmin,zmax = (1e9,1e12)
+        
+        print 'Plotting '+view+'...'
+        fig = pyplot.figure(1,(12,12))
+        fig.clf()
+        pyplot.imshow(z, extent=[x.min(),x.max(),y.min(),y.max()],
+                      cmap=cm.jet)
+        #pyplot.clim(numpy.log10(zmin),numpy.log10(zmax))
+        ax = pyplot.gca()
+        for sink in snap.sinks:
+            ax.plot(sink[1], -sink[0], 'k+', ms=7, mew=1.5) #90-degree rotation
+            ax.text(sink[1]+10, -sink[0]+5, '%.1f' %sink[3])
+        ax.set_xlim(x.min(),x.max())
+        ax.set_ylim(y.min(),y.max())
+        ax.set_xlabel('comoving kpc')
+        ax.set_ylabel('comoving kpc')
+        ax.text(-950,925,'z: %.2f' %snap.header.Redshift,
+                color='white',fontsize=18)
+        if t0:
+            t_acc = snap.header.Time*pyGadget.units.Time_yr - t0
+            ax.text(550,925,'t$_{form}$: %.1f yr' %t_acc,
+                    color='white',fontsize=18)
+        pyplot.draw()
+        pyplot.savefig(wpath, 
+                       bbox_inches='tight')
+    snap.close()
+
+def multitask(path, write_dir, start, stop,
+              boxsize,length_unit,pps,hsml_factor):
+    global t0
+    file_queue = Queue.Queue()
+    data_queue = Queue.Queue(3)
+    # Start the file-load thread
+    pyGadget.snapshot.Loader(load_dens, file_queue, data_queue).start()
+    # Add filenames to the queue
+    for i in xrange(start,stop+1):
+        fname = path + '{:0>3}'.format(i) + '.hdf5'
+        file_queue.put((fname,length_unit))
+    file_queue.put(None)
+    # Process images
+    while 1:
+        snapshot = data_queue.get()
+        if snapshot is None:
+            break # reached end of queue!
+        plot_dens(snapshot,write_dir,boxsize,length_unit,pps,hsml_factor)
+    print 'Done.'
+    
 #===============================================================================
-def py_scalar_map(pps,width, x,y,scalar_field,hsml,zshape):
-    zi = numpy.zeros(zshape)
-    nzi = numpy.zeros_like(zi)
-    i_min = (x - hsml + width/2.0) / width*pps
-    i_max = (x + hsml + width/2.0) / width*pps
-    j_min = (y - hsml + width/2.0) / width*pps
-    j_max = (y + hsml + width/2.0) / width*pps
-    weight = scalar_field*scalar_field
-    for n in range(scalar_field.size):
-        for i in xrange(pps):
-            if(i >= i_min[n] and i <= i_max[n]):
-                center_i = -width/2.0 + (i+0.5) * width/pps
-                for j in xrange(pps):
-                    if(j >= j_min[n] and j <= j_max[n]):
-                        center_j = -width/2.0 + (j+0.5) * width/pps
-                        r2 = ((x[n] - center_i)**2
-                              + (y[n] - center_j)**2) / hsml[n] / hsml[n]
-                        if(r2 <= 1.0):
-                            r = numpy.sqrt(r2)
-                            if(r <= 0.5):
-                                W_x = 1.0 - 6.0 * r**2 + 6.0 * r**3
-                            else:
-                                W_x = 2.0 * (1.0 - r)**3
-                            zi[i][j] += weight[n] * scalar_field[n] * W_x
-                            nzi[i][j] += weight[n] * W_x
-    return zi,nzi
+pyplot.ioff()
+if ((len(sys.argv) not in [2,3,4]) or (sys.argv[1] == '-h')):
+    print 'Usage::'
+    print '   Option 1: python gas_temp.py [simulation name] '\
+        '[beginning snapshot] [final snapshot]'
+    print '   Option 2: python gas_temp.py [simulation name] '\
+        '[single snapshot]'
+    print '   Option 3: python gas_temp.py [simulation name] '\
+        '(creates a plot for every snapshot)'
+    sys.exit()
 
-#===============================================================================
+simulation = sys.argv[1]
+path = os.getenv('HOME')+'/sim/'+simulation+'/snapshot_'
+sinkpath = os.getenv('HOME')+'/data/sinks/'+simulation+'/'
+write_dir = os.getenv('HOME')+'/data/simplots/'+simulation+'/'
+
 length_unit = pyGadget.units.Length_kpc
 pps = 1000 # 'pixels' per side
 hsml_factor = 1.7
-job_server = pp.Server()
-
-path = os.getenv('HOME')+'/sim/vanilla900/snapshot_'
-write_dir = os.getenv('HOME')+'/data/simplots/vanilla900/'
-suffix = '-structure.png'
 boxsize = 1e2/.71
-pyplot.ioff()
 
-#===============================================================================
-for snap in xrange(74,300):
-    fname = path + '{:0>3}'.format(snap) + '.hdf5'
+
+try:
+    sink = pyGadget.sinks.SingleSink(sinkpath)
+    t0 = sink.time[0]
+except IOError:
+    t0 = None
+
+if len(sys.argv) == 3:
+    snap = sys.argv[2]
+    fname = path + '{:0>3}'.format(snap)+'.hdf5'
     print 'loading', fname
-    snap = fname[-8:-5]
-    wpath = write_dir + snap + suffix
+    snapshot = load_dens(fname,length_unit)
+    plot_dens(snapshot,write_dir,boxsize,length_unit,pps,hsml_factor)
 
-    units = pyGadget.units
-    constants = pyGadget.constants
-    snapshot = pyGadget.snapshot.File(fname)
+elif len(sys.argv) == 4:
+    start = int(sys.argv[2])
+    stop = int(sys.argv[3])+1
+    multitask(path,write_dir,start,stop,
+              boxsize,length_unit,pps,hsml_factor)
 
-    # Read relevant attributes
-    h = snapshot.header.HubbleParam
-    a = snapshot.header.ScaleFactor
-    redshift = snapshot.header.Redshift
-
-    particle_mass = snapshot.gas.get_masses()
-    dens = snapshot.gas.get_number_density()
-    pos = snapshot.gas.get_coords(length_unit,comoving=True)
-    smL = snapshot.gas.get_smoothing_length(length_unit,comoving=True)
-    sinkval = snapshot.gas.get_sinks()
-    snapshot.close() 
-
-    # Initialization Complete --- Begin Analysis
-    print 'Analyzing...'
-    # Select only two highest resolution level particles
-    '''
-    minimum = numpy.amin(particle_mass)
-    refined = numpy.where(particle_mass <= 8.1*minimum)[0]
-    dens = dens[refined]
-    smL = smL[refined]
-    sinkval = sinkval[refined]
-    pos = pos[refined]
-    print 'Refinement complete.'
-    '''
-    width= boxsize
-    depth= width/10
-    center = pos[dens.argmax()]
-    x = pos[:,0] - center[0]
-    y = pos[:,1] - center[1]
-    z = pos[:,2] - center[2]
-
-    try: 
-        assert dens.max() <= 1e12
-    except AssertionError: 
-        print 'Warning: Maximum density exceeds 1e12 particles/cc!'
-        print 'Max Density: %.5e' %dens.max()
-    slice_ = numpy.where(numpy.abs(z) < depth/2)[0]
-    dens = dens[slice_]
-    smL = smL[slice_]
-    sinkval = sinkval[slice_]
-    x = x[slice_]
-    y = y[slice_]
-    z = z[slice_]
-    slice_ = numpy.where(numpy.abs(x) < width/2)[0]
-    dens = dens[slice_]
-    smL = smL[slice_]
-    sinkval = sinkval[slice_]
-    x = x[slice_]
-    y = y[slice_]
-    z = z[slice_]
-    slice_ = numpy.where(numpy.abs(y) < width/2)[0]
-    dens = dens[slice_]
-    smL = smL[slice_]
-    sinkval = sinkval[slice_]
-    x = x[slice_]
-    y = y[slice_]
-    z = z[slice_]
-    print 'Slicing complete.'
-    print ' x:: max: %.3e min: %.3e' %(x.max(),x.min())
-    print ' y:: max: %.3e min: %.3e' %(y.max(),y.min())
-    print ' density:: max: %.3e min: %.3e' %(dens.max(),dens.min())
-    print ' sink values:: max: %.3e min: %.3e' %(sinkval.max(),sinkval.min())
-    print ' smoothing length:: max: %.3e min: %.3e' %(smL.max(),smL.min())
-    print ' Array size:', dens.size
-
-    xres = yres = width/pps
-    xvals = numpy.arange(-width/2,width/2,xres)
-    yvals = numpy.arange(-width/2,width/2,yres)
-    xi,yi = numpy.meshgrid(xvals,yvals)
-    zi = numpy.zeros_like(xi)
-    nzi = numpy.zeros_like(zi)
-    zshape = zi.shape
-
-    hsml = numpy.fmax(hsml_factor * smL, width / pps / 2.0)
-    print 'Distributing...'
-    jobs = []
-    server_list = job_server.get_active_nodes()
-    ncpus = sum(server_list.values())
-    #Divide in to 16X as many tasks as there are cpus.
-    parts = ncpus*16
-    start = 0
-    end = dens.size - 1
-    step = (end - start) / parts + 1
-    print 'Dividing the input into',parts,'arrays of',step,'particles'
-    print 'for calculation on',ncpus,'cpus.'
-    for cpu in xrange(parts):
-        nstart = start+cpu*step
-        nend = min(start+(cpu+1)*step, end)
-        jobs.append(job_server.submit(scalar_map,
-                                      (pps,width, 
-                                       x[nstart:nend],
-                                       y[nstart:nend],
-                                       dens[nstart:nend],
-                                       hsml[nstart:nend],zshape),(),
-                                      ('numpy','from scipy import weave',
-                                       'from scipy.weave import converters')))
-    print 'Calculating...'
-    for job in jobs:
-        pzi,pnzi = job()
-        zi += pzi
-        nzi += pnzi
-
-    job_server.print_stats()
-    zi = numpy.where(nzi > 0, zi/nzi, zi)
-    #zi = numpy.fmax(zi, zmin)
-    #zi = numpy.fmin(zi, zmax)
-    #zi[0,0] = zmin
-    #zi[-1,-1] = zmax
-    zi = numpy.log10(zi)
-
-#===============================================================================
-    print 'Plotting...'
-    fig = pyplot.figure(1,(10,10))
-    fig.clf()
-    xi = xi*length_unit
-    yi = yi*length_unit
-    pyplot.imshow(zi, extent=[xi.min(),xi.max(),yi.min(),yi.max()])
-    #pyplot.colorbar()
-    ax = pyplot.gca()
-    ax.set_xlabel('comoving kpc')
-    ax.set_ylabel('comoving kpc')
-    ax.set_title('Redshift: %.5f' %(redshift,))
-#    pyplot.show()
-    pyplot.savefig(wpath, 
-                   bbox_inches='tight')
+else:
+    files0 = glob.glob(path+'???.hdf5')
+    files1 = glob.glob(path+'1???.hdf5')
+    files0.sort()
+    files1.sort()
+    start = int(files0[0][-8:-5])
+    stop = int(files0[-1][-8:-5])
+    if files1:
+        stop = int(files1[-1][-9:-5])
+        
+    multitask(path,write_dir,start,stop,
+              boxsize,length_unit,pps,hsml_factor)
