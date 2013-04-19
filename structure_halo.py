547d2ba553904ee091166fbd6365ceed9181602e
Jacob Hummel <jhummel@astro.as.utexas.edu>
2013-04-19 16:14:24 -0500

    Generalized the halo structure scripts. Now just specify scale on command line. 	renamed:    structure_halo500.py -> structure_halo.py 	deleted:    structure_halo10.py 	deleted:    structure_halo100.py 	deleted:    structure_halo1k.py 	deleted:    structure_halo1pc.py

--- structure_halo.py
+++ structure_halo.py
@@ -33,31 +33,32 @@ def load_dens(fname,length_unit):
 
     return snapshot
 
-def project(snap, write_dir, boxsize, length_unit, dlim, np, pps, sm):
+def project(snap, write_dir, scale, cscale, dlim, np, pps, sm):
     global t0
-    for suffix in ['-halo1-xy.png']:
+    boxsize = int("".join(ch if ch.isdigit() else "" for ch in scale))
+    unit = "".join(ch if not ch.isdigit() else "" for ch in scale)
+    length_unit = pyGadget.units.Lengths[unit]
+
+    #(Scaling boxsize to physical size at redshift 25.)
+    boxsize = 25 * boxsize / (1 + snap.header.Redshift)
+    for suffix in ['-halo500-xy.png']:
         wpath = write_dir + '{:0>4}'.format(snap.number) + suffix
         view = suffix[-6:-4]
         x,y,z = pyGadget.visualize.density_projection(snap, view, boxsize, 1., 
                                                       length_unit, 'halo',
                                                       dlim, np, pps, sm)
         z = numpy.log10(z)
-        #set colorbar limits
-        zmax = z.max()
-        zmin = z.min()
-        zmax = 9
-        zmin = 3.5
         print 'Plotting '+view+'...'
         fig = pyplot.figure(1,(16,12))
         fig.clf()
         pyplot.imshow(z, extent=[x.min(),x.max(),y.min(),y.max()], cmap=cm.jet)
-        pyplot.clim(zmin,zmax)
+        pyplot.clim(cscale[0],cscale[1])
         pyplot.colorbar()
         ax = pyplot.gca()
         ax.set_xlim(x.min(),x.max())
         ax.set_ylim(y.min(),y.max())
-        ax.set_xlabel('Physical Distance [pc]')
-        ax.set_ylabel('Physical Distance [pc]')
+        ax.set_xlabel('Physical Distance ['+unit+']')
+        ax.set_ylabel('Physical Distance ['+unit+']')
         ax.text(-950,925,'z: %.2f' %snap.header.Redshift,
                 color='white',fontsize=18)
         if t0:
@@ -70,9 +71,11 @@ def project(snap, write_dir, boxsize, length_unit, dlim, np, pps, sm):
                        bbox_inches='tight')
     snap.close()
 
-def multitask(path, write_dir, start, stop, boxsize, length_unit, 
-              dlim, np, pps, sm):
+def multitask(path, write_dir, start, stop, scale, cscale, dlim, np, pps, sm):
     global t0
+    unit = "".join(ch if not ch.isdigit() else "" for ch in scale)
+    length_unit = pyGadget.units.Lengths[unit]
+
     file_queue = Queue.Queue()
     data_queue = Queue.Queue(3)
     # Start the file-load thread
@@ -87,18 +90,17 @@ def multitask(path, write_dir, start, stop, boxsize, length_unit,
         snapshot = data_queue.get()
         if snapshot is None:
             break # reached end of queue!
-        box = boxsize / (1 + snapshot.header.Redshift)
-        project(snapshot,write_dir,box,length_unit,dlim,np,pps,sm)
+        project(snapshot,write_dir,scale,cscale,dlim,np,pps,sm)
     print 'Done.'
     
 #===============================================================================
-if ((len(sys.argv) not in [2,3,4]) or (sys.argv[1] == '-h')):
+if ((len(sys.argv) not in [3,4,5]) or (sys.argv[1] == '-h')):
     print 'Usage::'
-    print '   Option 1: python gas_temp.py [simulation name] '\
+    print '   Option 1: python gas_temp.py [simulation name] [length scale] '\
         '[beginning snapshot] [final snapshot]'
-    print '   Option 2: python gas_temp.py [simulation name] '\
+    print '   Option 1: python gas_temp.py [simulation name] [length scale] '\
         '[single snapshot]'
-    print '   Option 3: python gas_temp.py [simulation name] '\
+    print '   Option 1: python gas_temp.py [simulation name] [length scale] '\
         '(creates a plot for every snapshot)'
     sys.exit()
 
@@ -107,32 +109,33 @@ path = os.getenv('HOME')+'/sim/'+simulation+'/snapshot_'
 sinkpath = os.getenv('HOME')+'/data/sinks/'+simulation+'/'
 write_dir = os.getenv('HOME')+'/data/simplots/'+simulation+'/'
 
-length_unit = pyGadget.units.Length_pc
-pps = 500 # 'pixels' per side
-sm = 1.7
-boxsize = 1*25
-dlim = 1e6
-np = 1000
-
-
+colors = {'1kpc': (-2.5,3), '1000pc':(-2.5,3), '500pc':(-2.5,3), 
+          '100pc':(-1,3.5), '10pc':(1,6), '1pc':(3.5,9)}
+scaling = sys.argv[2]
+cscaling = colors[scaling]
+pps = 500   # 'pixels' per side
+sm = 1.7    # smoothing factor
+dlim = 1e6  # density limit for finding halo center
+np = 1000   # number of particles to require for finding halo center
 try:
     sink = pyGadget.sinks.SingleSink(sinkpath)
     t0 = sink.time[0]
 except IOError:
     t0 = None
 
-if len(sys.argv) == 3:
-    snap = sys.argv[2]
+if len(sys.argv) == 4:
+    snap = sys.argv[3]
     fname = path + '{:0>3}'.format(snap)+'.hdf5'
     print 'loading', fname
+    unit = "".join(ch if not ch.isdigit() else "" for ch in scaling)
+    length_unit = pyGadget.units.Lengths[unit]
     snapshot = load_dens(fname,length_unit)
-    boxsize = boxsize / (1 + snapshot.header.Redshift)
-    project(snapshot,write_dir,boxsize,length_unit,dlim,np,pps,sm)
+    project(snapshot,write_dir,scaling,cscaling,dlim,np,pps,sm)
 
-elif len(sys.argv) == 4:
-    start = int(sys.argv[2])
-    stop = int(sys.argv[3])
-    multitask(path,write_dir,start,stop,boxsize,length_unit,dlim,np,pps,sm)
+elif len(sys.argv) == 5:
+    start = int(sys.argv[3])
+    stop = int(sys.argv[4])
+    multitask(path,write_dir,start,stop,scaling,cscaling,dlim,np,pps,sm)
 
 else:
     files0 = glob.glob(path+'???.hdf5')
@@ -143,5 +146,4 @@ else:
     stop = int(files0[-1][-8:-5])
     if files1:
         stop = int(files1[-1][-9:-5])
-        
-    multitask(path,write_dir,start,stop,boxsize,length_unit,dlim,np,pps,sm)
+    multitask(path,write_dir,start,stop,scaling,cscaling,dlim,np,pps,sm)
