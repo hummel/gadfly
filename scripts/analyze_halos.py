8006ed4dfb5d277e0588326694d8c23318ec3688
Jacob Hummel <jhummel@astro.as.utexas.edu>
2014-04-14 23:19:17 -0500

    Updated script. 	modified:   analyze_halos.py

--- scripts/analyze_halos.py
+++ scripts/analyze_halos.py
@@ -1,11 +1,13 @@
 # halos.py
 # Jacob Hummel
 # quick script for updating all halo databases.
+import sys
 import pyGadget
 
-for simulation in ['stampede/vanilla', 'stampede/XR_sfr_1e-1',
-                   'stampede/XR_sfr_1e-2', 'stampede/XR_sfr_1e-3']:
-    sim = pyGadget.sim.Simulation(simulation)
-    halo = pyGadget.halo.Halo(sim)
-    for snap in sim.snapfiles.keys():
-        halo.populate(snap)
+simulation = sys.argv[1]
+sim = pyGadget.sim.Simulation(simulation)
+halo = pyGadget.halo.Halo(sim)
+keys = sim.snapfiles.keys()
+keys.sort()
+for snap in keys:
+    halo.load(snap)
